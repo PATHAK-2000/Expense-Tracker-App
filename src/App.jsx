f21290d4e04bd9e56299874f1644fe76cd5292e5
Tej Patel <tejp@webmobtech.com>
2023-03-06 16:51:24 +0530

    Added useContext and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -7,17 +7,40 @@ import NavBar from "./components/NavBar";
 import CreateExpense from "./pages/CreateExpense/CreateExpense";
 import TotalExpense from "./pages/TotalExpense/TotalExpense";
 import ViewExpense from "./pages/ViewExpense/ViewExpense";
+import UserContext from "./Context";
 function App() {
+  const [selectType, setSelectType] = useState("");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [finalData, setFinalData] = useState({
+    type: "",
+    description: "",
+    amount: "",
+  });
+
   return (
     <div className="App">
       <div className="container">
         <Router>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<CreateExpense />} />
-            <Route path="/view" element={<ViewExpense />} />
-            <Route path="/total" element={<TotalExpense />} />
-          </Routes>
+          <UserContext.Provider
+            value={{
+              selectType,
+              setSelectType,
+              description,
+              setDescription,
+              amount,
+              setAmount,
+              finalData,
+              setFinalData,
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<CreateExpense />} />
+              <Route path="/view" element={<ViewExpense />} />
+              <Route path="/total" element={<TotalExpense />} />
+            </Routes>
+          </UserContext.Provider>
         </Router>
       </div>
     </div>
--- src/pages/CreateExpense/CreateExpense.jsx
+++ src/pages/CreateExpense/CreateExpense.jsx
@@ -1,41 +1,124 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import "./create-expense.css";
+
 import More from "../../assets/ExpandMore.svg";
+import Less from "../../assets/ExpandLess.svg";
+import UserContext from "../../Context";
+
 const CreateExpense = () => {
-  const [dropMenu, setDropMenu] = useState(false);
+  const [dropMenu, setDropMenu] = useState(true);
+
+  const {
+    selectType,
+    setSelectType,
+    description,
+    setDescription,
+    amount,
+    setAmount,
+    finalData,
+    setFinalData,
+  } = useContext(UserContext);
+  let menuRef = useRef();
 
   let handleClick = () => {
     setDropMenu(!dropMenu);
   };
 
+  let selectItems = (e) => {
+    setSelectType(e.target.getAttribute("value"));
+    setDropMenu(!dropMenu);
+  };
+
+  // Desription change
+  let handleDesctiptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  // Amount change
+  let handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  let handleSubmit = () => {
+    setFinalData({type: selectType, description: description, amount: amount})
+  };
+  
+
+  useEffect(() => {
+    selectItems;
+  }, [selectType]);
+
+  // For closing pop-up on clicking outside.
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setDropMenu(true);
+      }
+    };
+
+    document.addEventListener("click", handler);
+  }, []);
+
   return (
     <div className="expense__main">
       <div className="expense__type">
-        <div className="selection" onClick={handleClick}>
+        <div className="type__label">
+          <label>Select Type:</label>
+        </div>
+        <div className="selection" onClick={handleClick} ref={menuRef}>
           <div className="selection__name">
             <h1> SELECT</h1>
           </div>
           <div className="selection__img">
-            <img src={More} alt="Expand-More" className="expand-more" />
+            {dropMenu ? (
+              <img src={More} alt="Expand-More" className="expand" />
+            ) : (
+              <img src={Less} alt="Expand-Less" className="expand" />
+            )}
           </div>
         </div>
-        <div className={dropMenu ? "dropDown__hidden" : "dropDown__visible"}>
-          <h1>HOME</h1>
-          <h1>TRAVEL</h1>
-          <h1>FOOD</h1>
-          <h1>MISC</h1>
+
+        <div
+          className={dropMenu ? "dropDown__hidden" : "dropDown__visible"}
+          onClick={selectItems}
+        >
+          <h1 value="home">HOME</h1>
+          <h1 value="travel">TRAVEL</h1>
+          <h1 value="food">FOOD</h1>
+          <h1 value="misc">MISC</h1>
         </div>
       </div>
+
       <div className="expense__description">
+        <div className="desctiption__label">
+          <label>Description:</label>
+        </div>
         <input
           type="text"
           name="description"
           id="description"
           placeholder="Description..."
+          onChange={handleDesctiptionChange}
+          value={description}
         />
       </div>
+
       <div className="expense__amount">
-        <input type="number" name="amount" id="amount" placeholder="Amount" />
+        <div className="amount__label">
+          <label>Amount:</label>
+        </div>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          placeholder="Amount"
+          onChange={handleAmountChange}
+          value={amount}
+        />
+      </div>
+
+      <div className="expense__submit">
+        <button onClick={handleSubmit}>Submit</button>
       </div>
     </div>
   );
